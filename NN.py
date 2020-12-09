ff9682b4f7e6dd8c57eca53c302e22dddec21ee9
Martin Sig Nørbjerg <Martin1509@outlook.dk>
2020-12-09 10:54:26 +0100

    Opdaterede koden

--- NN.py
+++ NN.py
@@ -2,43 +2,39 @@ import numpy as np
 from misc import computeDistance
 
 def nearestNeighbour (weightMatrix: np.array, startingIndex: int) -> []:
-    """ Find en hamilton kreds ved hjælp af nærmeste nabo algoritmen """
+    """ Find en hamilton kreds ved hjaelp af naermeste nabo algoritmen """
     path = [startingIndex]
-
     n = np.shape(weightMatrix)[0] # Antallet af punkter
 
-    # Loop ind til alle punkter er tilføjet
+    # Loop ind til alle punkter er tilfoejet
     while (len(path) < n):
 
-        # Find den punktet tættest på det nuværende:
+        # Find den punktet taettest på det nuvaerende:
         nearest = None
-        previusNode = path[-1] - 1 # Det sidst besøgte punkts index i matricen
+        previusNode = path[-1] - 1 # Det sidst besoegte punkts index i matricen
 
         for i in range(n):
             if (((i + 1) in path) == False): # Kig kun på punkter der ikke allerede er i vejen.
-                if (nearest == None): nearest = i # Hvis det er det første ikke besøgte punkt der kigges på.
+                if (nearest == None): nearest = i # Hvis det er det foerste ikke besoegte punkt der kigges på.
                 else:
                     if (weightMatrix[previusNode][i] < weightMatrix[previusNode][nearest]): nearest = i
 
-        # Tilføj det tætteste punkt til vejen
-        path.append(nearest + 1)
-
-
-    return path + [startingIndex] # Gå tilbage til det første punkt
+        path.append(nearest + 1) # Tilfoej det taetteste punkt til vejen 
+        
+    return path + [startingIndex] # Gae tilbage til det første punkt
 
 if (__name__ == "__main__"):
-    # Vægt matrice 
-    W = [[   np.inf, 15,     51,     71,     97,     58,     46,     7,      16,     37],
-         [   15,     np.inf, 42,     66,     100,    60,     57,     20,     31,     48],
-         [   51,     42,     np.inf, 29,     74,     39,     44,     49,     64,     88],
-         [   71,     66,     29,     np.inf, 49,     27,     41,     66,     80,    107],
-         [   97,     100,    74,     49,     np.inf, 40,     51,     90,     99,    126],
-         [   58,     60,     39,     27,     40,     np.inf, 17,     52,     63,     91],
-         [   46,     51,     44,     41,     51,     17,     np.inf, 39,     48,     76],
-         [    7,     20,     49,     66,     90,     52,     39,     np.inf, 15,     41],
-         [   16,     31,     64,     80,     99,     63,     48,     15,     np.inf, 28],
-         [   37,     48,     88,     107,    126,    91,     76,     41,     28,     np.inf]]
-    
+    # Vaegt matrix 
+    W = [[np.inf,15,51,71,97,58,46,7,16,37],
+         [15,np.inf,42,66,100,60,57,20,31,48],
+         [51,42,np.inf,29,74,39,44,49,64,88],
+         [71,66,29,np.inf,49,27,41,66,80,107],
+         [97,100,74,49,np.inf,40,51,90,99,126],
+         [58,60,39,27,40,np.inf,17,52,63,91],
+         [46,51,44,41,51,17,np.inf,39,48,76],
+         [ 7,20,49,66,90,52,39,np.inf,15,41],
+         [16,31,64,80,99,63,48,15,np.inf,28],
+         [37,48,88,107,126,91,76,41,28,np.inf]]
     C = nearestNeighbour(np.array(W), 1)
     print("C: {0}, D(C): {1}".format(C, computeDistance(W, C)))
     # OUTPUT: C: [1, 8, 9, 10, 2, 3, 4, 6, 7, 5, 1], D(C): 355
--- bruteforce.py
+++ bruteforce.py
@@ -2,16 +2,17 @@ from itertools import permutations
 from misc import computeDistance
 
 def bruteForce (W: [[float]], nodes: [int]) -> ([int]):
-    """ Bruteforce TSP solver """
-    # Genere alle mulige hamilton kredse
+    """ Bruteforce løsning af den handelsrejsendes problem """
+    # Genere samtlige mulige hamilton kredse 
+    # Denne tjekker den samme hamilton kreds flere gange (baglaens og forlaens)
     curcuits = [list(P) + [P[0]] for P in permutations(nodes)]
 
     n = len(curcuits) # Antallet af kredse
     print("Posible Hamilton curcuits: {0}".format(n))
 
-    # Tjek den første af hamilton kredsene
+    # Tjek den foerste af hamilton kredsene
     bestCurcuit = curcuits[0]
-    bestDistance = computeDistance(W, bestCurcuit) # NOTE: Finder en vejens vægt.
+    bestDistance = computeDistance(W, bestCurcuit) # Finder vejens vaegt.
 
     # Tjekker de resterende hamilton kredse igennem
     for index, c in enumerate(curcuits[1:]):
@@ -23,16 +24,17 @@ def bruteForce (W: [[float]], nodes: [int]) -> ([int]):
     return bestCurcuit
 
 if (__name__ == "__main__"):
-    W = [[   np.inf, 15,     51,     71,     97,     58,     46,     7,      16,     37],
-         [   15,     np.inf, 42,     66,     100,    60,     57,     20,     31,     48],
-         [   51,     42,     np.inf, 29,     74,     39,     44,     49,     64,     88],
-         [   71,     66,     29,     np.inf, 49,     27,     41,     66,     80,    107],
-         [   97,     100,    74,     49,     np.inf, 40,     51,     90,     99,    126],
-         [   58,     60,     39,     27,     40,     np.inf, 17,     52,     63,     91],
-         [   46,     51,     44,     41,     51,     17,     np.inf, 39,     48,     76],
-         [    7,     20,     49,     66,     90,     52,     39,     np.inf, 15,     41],
-         [   16,     31,     64,     80,     99,     63,     48,     15,     np.inf, 28],
-         [   37,     48,     88,     107,    126,    91,     76,     41,     28,     np.inf]]
+    # Vaegt matrix
+    W = [[np.inf,15,51,71,97,58,46,7,16,37],
+         [15,np.inf,42,66,100,60,57,20,31,48],
+         [51,42,np.inf,29,74,39,44,49,64,88],
+         [71,66,29,np.inf,49,27,41,66,80,107],
+         [97,100,74,49,np.inf,40,51,90,99,126],
+         [58,60,39,27,40,np.inf,17,52,63,91],
+         [46,51,44,41,51,17,np.inf,39,48,76],
+         [ 7,20,49,66,90,52,39,np.inf,15,41],
+         [16,31,64,80,99,63,48,15,np.inf,28],
+         [37,48,88,107,126,91,76,41,28,np.inf]]
 
     nodes = list(range(1, 11))
     C = bruteForce(W, nodes)
